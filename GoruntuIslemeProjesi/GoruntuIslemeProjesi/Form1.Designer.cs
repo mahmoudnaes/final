e166ad18ae63d0b06cc8f70303114728db218997
Mahmoud <m.a.naes1996@hotmail.com>
2019-12-22 21:12:03 +0300

    Goruntu Isleme Projesi

---
+++ GoruntuIslemeProjesi/GoruntuIslemeProjesi/Form1.Designer.cs
@@ -0,0 +1,113 @@
+﻿namespace GoruntuIslemeProjesi
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.start = new System.Windows.Forms.Button();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.Stop = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(12, 55);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(434, 429);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Location = new System.Drawing.Point(477, 55);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(434, 429);
+            this.pictureBox2.TabIndex = 1;
+            this.pictureBox2.TabStop = false;
+            // 
+            // start
+            // 
+            this.start.Location = new System.Drawing.Point(290, 26);
+            this.start.Name = "start";
+            this.start.Size = new System.Drawing.Size(75, 23);
+            this.start.TabIndex = 3;
+            this.start.Text = "Başla";
+            this.start.UseVisualStyleBackColor = true;
+            this.start.Click += new System.EventHandler(this.start_Click);
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(12, 26);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(272, 21);
+            this.comboBox1.TabIndex = 4;
+            this.comboBox1.Text = "Kamera Seç";
+            // 
+            // Stop
+            // 
+            this.Stop.Location = new System.Drawing.Point(372, 26);
+            this.Stop.Name = "Stop";
+            this.Stop.Size = new System.Drawing.Size(75, 23);
+            this.Stop.TabIndex = 5;
+            this.Stop.Text = "Kapat";
+            this.Stop.UseVisualStyleBackColor = true;
+            this.Stop.Click += new System.EventHandler(this.Stop_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(984, 632);
+            this.Controls.Add(this.Stop);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.start);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button start;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button Stop;
+    }
+}
+
---
+++ GoruntuIslemeProjesi/GoruntuIslemeProjesi/Form1.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using AForge;
+using AForge.Imaging;
+using AForge.Video;
+using AForge.Video.DirectShow;
+using AForge.Imaging.Filters;
+
+
+
+namespace GoruntuIslemeProjesi
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        private FilterInfoCollection CaptureDevices;
+        private VideoCaptureDevice videoSource;
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            CaptureDevices = new FilterInfoCollection(FilterCategory.VideoInputDevice);
+            foreach (FilterInfo Device in CaptureDevices)
+            {
+                comboBox1.Items.Add(Device.Name);
+            }
+            comboBox1.SelectedIndex = 0;
+            videoSource = new VideoCaptureDevice();
+        }
+
+        private void start_Click(object sender, EventArgs e)
+        {
+            videoSource = new VideoCaptureDevice(CaptureDevices[comboBox1.SelectedIndex].MonikerString);
+            videoSource.NewFrame += new NewFrameEventHandler(VideoSource_NewFrame);
+            videoSource.Start();
+        }
+
+        private void VideoSource_NewFrame(object sender, NewFrameEventArgs eventArgs)
+        {
+            //resimler tanimlama
+            Bitmap image = (Bitmap)eventArgs.Frame.Clone();
+            Bitmap image1 = (Bitmap)eventArgs.Frame.Clone();
+            Bitmap image2 = (Bitmap)eventArgs.Frame.Clone();
+
+            //aynalama uygulamasi
+            Mirror ayna = new Mirror(false, true);
+            ayna.ApplyInPlace(image);
+            ayna.ApplyInPlace(image1);
+            ayna.ApplyInPlace(image2);
+            
+            //orginal video picturebox1 aktarmasi
+            pictureBox1.Image = image;
+
+            //euclidean filtresi uygulamasi
+            EuclideanColorFiltering filter = new EuclideanColorFiltering();
+            filter.CenterColor = new RGB(Color.FromArgb(0, 152, 125)); //mavi ve yesil icin
+            filter.Radius = 100;
+            filter.ApplyInPlace(image1);
+            //filter.CenterColor = new RGB(Color.FromArgb(0, 140, 10)); //yesil icin
+            filter.CenterColor = new RGB(Color.FromArgb(170, 0, 0)); //kirmizi icin
+            filter.Radius = 100;
+            filter.ApplyInPlace(image2);
+            //toplama filtresi uygulamasi
+            Add topla = new Add(image2);
+            Bitmap resultImage = topla.Apply(image1);
+
+            //sonucu picturebox2 aktarmasi
+            pictureBox2.Image = resultImage;
+
+
+            
+
+        }
+        
+        private void Stop_Click(object sender, EventArgs e)
+        {
+            if (videoSource.IsRunning==true)
+            {
+                videoSource.Stop();
+            }
+            Application.Exit(null);
+        }
+    }
+}
